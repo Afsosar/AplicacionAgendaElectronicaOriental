1e2648c91a9117c7a3c5d43ac08a39f33dcb20a3
alan fabricio <afsosa01@yahoo.es>
2018-03-03 18:33:54 -0600

    Llenado del cardView_Categorias desde la base de datos y modificación del layout card_item_categorias.

--- app/src/main/java/unah/proyecto/aeo/aplicacionagendaelectronicaoriental/clasesJAVAAlan/ActivityCategorias.java
+++ app/src/main/java/unah/proyecto/aeo/aplicacionagendaelectronicaoriental/clasesJAVAAlan/ActivityCategorias.java
@@ -2,6 +2,8 @@ package unah.proyecto.aeo.aplicacionagendaelectronicaoriental.clasesJAVAAlan;
 
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -23,8 +25,8 @@ import unah.proyecto.aeo.aplicacionagendaelectronicaoriental.clasesJAVAVirgilio.
 
 public class ActivityCategorias extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-
-    private ListView lista;
+    ArrayList<Fuente_Categoria> lista;
+    ConexionSQLiteHelper conn;
 
 
     @Override
@@ -32,36 +34,30 @@ public class ActivityCategorias extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_categorias);
 
+        //Conexión a la base de datos
+        conn = new ConexionSQLiteHelper(this,"bdaeo",null,1);
 
+        //Inicializacion del array
+        lista= new ArrayList<Fuente_Categoria>();
 
-        ConexionSQLiteHelper conn=new ConexionSQLiteHelper(this,"bd_aeo",null,1);
-
-        ArrayList<Fuente_Categoria>lista= new ArrayList<Fuente_Categoria>();
-        lista.add(new Fuente_Categoria("Emergencia", R.drawable.casa,65,0));
-        lista.add(new Fuente_Categoria("Hospitales",R.drawable.ho,5,0));
-        lista.add(new Fuente_Categoria("Clinicas",R.drawable.casa,23,0));
-        lista.add(new Fuente_Categoria("Escuelas",R.drawable.casa,45,0));
-        lista.add(new Fuente_Categoria("Univercidades",R.drawable.ho,6,0));
-        lista.add(new Fuente_Categoria("Museos",R.drawable.casa,4,0));
-        lista.add(new Fuente_Categoria("Ferreterias",R.drawable.casa,20,0));
-        lista.add(new Fuente_Categoria("Agrocomerciales",R.drawable.ho,80,0));
-        lista.add(new Fuente_Categoria("Laboratorios",R.drawable.casa,28,0));
-
-
-
+        //Inicializacion del RecyclerView
         RecyclerView contenedor = (RecyclerView) findViewById(R.id.contenedor);
         contenedor.setHasFixedSize(true);
         LinearLayoutManager layout = new LinearLayoutManager(getApplicationContext());
         layout.setOrientation(LinearLayoutManager.VERTICAL);
-        contenedor.setAdapter(new Adaptador_Categoria(lista));
+
+        //Llamada al método para consultar la base de datos
+        consultarListaCategorias();
+
+
+        //Declaracion y seteo del adaptador al contenedor
+        Adaptador_Categoria adaptador_categoria  = new Adaptador_Categoria(lista);
+        contenedor.setAdapter(adaptador_categoria);
         contenedor.setLayoutManager(layout);
 
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -123,6 +119,31 @@ public class ActivityCategorias extends AppCompatActivity
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+
+    //Metodo que consulta a la base de datos para ver las  categorias
+    private void consultarListaCategorias(){
+
+        //Obtener la base de datos
+        SQLiteDatabase db = conn.getReadableDatabase();
+
+        Fuente_Categoria fuente_categoria = null;
+
+        //Asignar la consulta sql
+        Cursor cursor =  db.rawQuery("SELECT nombre_categoria, imagen_categoria FROM CATEGORIAS",null);
+
+        //se obtienen los objetos de la consulta y se asignan a los componentes visuales
+        while (cursor.moveToNext()){
+            fuente_categoria = new Fuente_Categoria();
+            fuente_categoria.setTitulo(cursor.getString(0));
+            fuente_categoria.setImagen(cursor.getInt(1));
+
+
+            //se añade los datos al array
+            lista.add(fuente_categoria);
+
+        }
     }
 
 
--- app/src/main/java/unah/proyecto/aeo/aplicacionagendaelectronicaoriental/clasesJAVAAlan/Fuente_Categoria.java
+++ app/src/main/java/unah/proyecto/aeo/aplicacionagendaelectronicaoriental/clasesJAVAAlan/Fuente_Categoria.java
@@ -8,15 +8,19 @@ public class Fuente_Categoria {
     String Titulo;
     int imagen;
     int cantidad;
+    int id;
     int estado;
 
-    public Fuente_Categoria(String titulo, int imagen, int cantidad, int estado) {
+    public Fuente_Categoria(String titulo, int imagen, int cantidad,int id,int estado) {
         Titulo = titulo;
         this.imagen = imagen;
         this.cantidad = cantidad;
+        this.id= id;
         this.estado = estado;
     }
+public Fuente_Categoria(){
 
+}
     public String getTitulo() {
         return Titulo;
     }
@@ -39,6 +43,14 @@ public class Fuente_Categoria {
 
     public void setCantidad(int cantidad) {
         this.cantidad = cantidad;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public int getEstado() {
